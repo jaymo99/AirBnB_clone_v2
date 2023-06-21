8faaa71a06c3e1861b3b891ae6808cfc8e7b09e0
James Kariuki <karisjaymo99@gmail.com>
2023-06-21 22:14:13 +0300

    add db_storage engine. update amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''represents an amenity for a place.'''
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship(
+            )
