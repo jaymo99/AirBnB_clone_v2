b8c0cd4b22f57e6b28a2a4001db005b5ce9df179
James Kariuki <karisjaymo99@gmail.com>
2023-07-08 18:49:14 +0300

    add '0-setup_web_static.sh'

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Script sets up a web server for the deployment of 'web_static'
+
+sudo apt-get -y update && sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+mkdir -p /data/web_static/releases/test/ \
+	&& touch /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+
+printf "<html>\n\
+\t<head>\n\
+\t\t<body>\n\
+\t\t\tHolberton School\n\
+\t\t</body>\n\
+\t</head>\n\
+</html>" > /data/web_static/releases/test/index.html
+
+# Remove symbolic link if it exists
+if [ -L "/data/web_static/current" ]; then
+	rm /data/web_static/current
+fi
+
+# Create symbolic link
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+hbnb_static="
+	location \/hbnb_static {
+		alias \/data\/web_static\/current\/;
+	}
+"
+sudo sed -i "/server_name _/ a ${hbnb_static}" /etc/nginx/sites-available/default
+sudo sed -i "/server_name jmkariuki.tech/ a ${hbnb_static}" /etc/nginx/sites-available/default
+sudo service nginx restart
