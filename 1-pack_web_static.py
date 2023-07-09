1dd666955c516b1786c09dff92adb06ed3268edd
James Kariuki <karisjaymo99@gmail.com>
2023-07-09 21:58:53 +0300

    add '1-pack_web_static.py'

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module uses Fabric for web_static compression
+'''
+
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    '''generates a .tgz archive from the contents of
+    web_static folder
+    '''
+    local("mkdir -p versions")
+    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    zip_path = "versions/web_static_{}.tgz".format(time_stamp)
+    status = local("tar -czvf {} web_static".format(zip_path), capture=True)
+    if status.failed:
+        return None
+    return zip_path
