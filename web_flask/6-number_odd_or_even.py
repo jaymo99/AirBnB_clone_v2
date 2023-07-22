f309f50531dbdaedb94600f29eb897f9d51ce6ab
James Kariuki <karisjaymo99@gmail.com>
2023-07-22 22:07:15 +0300

    add route 'number_is_even_or_odd'

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,5 +58,17 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    '''number_template route
+    Renders a html page only if 'n' is an integer
+    '''
+    if n % 2 == 0:
+        status = 'even'
+    else:
+        status = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, status=status)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
