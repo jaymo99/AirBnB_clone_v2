53e0ed6eaea25bd964ef7c928630e027564e0a1b
James Kariuki <karisjaymo99@gmail.com>
2023-07-23 12:43:57 +0300

    add '7-states_list.py'

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""7-states_list module
+Starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def clean_up():
+    '''
+    Cleans-up app resources after request
+    '''
+    print("Cleaning up after request...")
+    storage.close()
+
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    '''
+    Renders a html page with a list of states
+    '''
+    states = list(storage.all().values())
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
